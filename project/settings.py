6677e914fbfb69e522ae11a3491c4879d25ad463
Diego Freire <freirediego128@gmail.com>
2023-08-08 15:59:27 -0300

    fourth commit

--- project/settings.py
+++ project/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'user/templates/user/static')]
 
